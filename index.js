7edd9334582f35c411601cc85227f2a949713df0
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-11-27 21:59:41 +0600

    jwt token function set

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ app.use(cors())
 app.use(express.json())
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hymefqt.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJwtToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run() {
     try {
         const samsungCollection = client.db('usedMobile').collection('samsung');
