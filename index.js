f7bfa5291125f2baf375589c7755ff159f9e2355
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-11-27 16:46:43 +0600

    get user information

--- index.js
+++ index.js
@@ -66,6 +66,12 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users)
+        });
+
     }
     finally { }
 }
